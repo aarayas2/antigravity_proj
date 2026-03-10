4c7b5749b89728178de2303cd1f96c8c7b162e9c
aarayas <aarayas@hotmail.com>
2026-03-10 11:46:25 -0700

    rearrange RSI, BB, ...

--- stock-analysis/strategy/__init__.py
+++ stock-analysis/strategy/__init__.py
@@ -4,11 +4,11 @@ from .RSI import apply_strategy as rsi_apply, needs_subplots as rsi_needs_subplo
 from .MACD import apply_strategy as macd_apply, needs_subplots as macd_needs_subplots, add_traces as macd_add_traces, get_signals as macd_get_signals
 
 STRATEGIES = {
-    "SMA Crossover": {
-        "apply_strategy": sma_apply,
-        "needs_subplots": sma_needs_subplots,
-        "add_traces": sma_add_traces,
-        "get_signals": sma_get_signals
+    "RSI": {
+        "apply_strategy": rsi_apply,
+        "needs_subplots": rsi_needs_subplots,
+        "add_traces": rsi_add_traces,
+        "get_signals": rsi_get_signals
     },
     "Bollinger Bands": {
         "apply_strategy": bb_apply,
@@ -16,16 +16,16 @@ STRATEGIES = {
         "add_traces": bb_add_traces,
         "get_signals": bb_get_signals
     },
-    "RSI": {
-        "apply_strategy": rsi_apply,
-        "needs_subplots": rsi_needs_subplots,
-        "add_traces": rsi_add_traces,
-        "get_signals": rsi_get_signals
-    },
     "MACD": {
         "apply_strategy": macd_apply,
         "needs_subplots": macd_needs_subplots,
         "add_traces": macd_add_traces,
         "get_signals": macd_get_signals
+    },
+    "SMA Crossover": {
+        "apply_strategy": sma_apply,
+        "needs_subplots": sma_needs_subplots,
+        "add_traces": sma_add_traces,
+        "get_signals": sma_get_signals
     }
 }
